fc4fd407c9ed016cbb15b4b122f0d3ab1ebd7ff1
Jean Cerqueira <74473537+jeancds29@users.noreply.github.com>
2021-06-10 16:24:09 -0300

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,19 @@
-var express = require("express")
-var app = express();
-var router = express.Router();
-var HomeController = require("../controllers/HomeController");
+const express = require("express")
+const app = express();
+const router = express.Router();
+const HomeController = require("../controllers/HomeController");
+const UserController = require("../controllers/UserController");
+const AdminAuth = require("../middleware/AdminAuth");
+
 
 router.get('/', HomeController.index);
+router.post('/user', UserController.signUp);
+router.get('/user',AdminAuth, UserController.index);
+router.get('/user/:id', AdminAuth,UserController.findUser);
+router.put('/user',AdminAuth, UserController.update);
+router.delete('/user/:id',AdminAuth, UserController.delete);
+router.post('/recoverpassword', UserController.recoverPassword);
+router.post('/changepassword', UserController.changePassword);
+router.post('/login', UserController.login);
 
 module.exports = router;
